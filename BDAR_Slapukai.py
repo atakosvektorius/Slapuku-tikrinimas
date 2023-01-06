bd395e728d477b6aa5153c5676b5c1bad1313dbc
Tomas Savenas <savenas@users.noreply.github.com>
2023-01-06 14:19:38 +0200

    fixing major vulnerabilities

--- BDAR_Slapukai.py
+++ BDAR_Slapukai.py
@@ -13,26 +13,24 @@ class bcolors:
     UNDERLINE = '\033[4m'
 
 
-
-
-
 def open_website_links(file_name):
   with open(file_name, 'r') as file:
     for line in file:
       try:
         options = Options()
-        options = Options()
         options.add_argument("--headless")
         options.add_argument("--start-maximized")
         options.add_argument("--disable-notifications")
         options.add_argument("--incognito")
-        driver = webdriver.Chrome(chrome_options=options)
+        driver = webdriver.Chrome(options=options)
         driver.set_page_load_timeout(10)
         url = line.rstrip()
         driver.get("https://"+url)
         cookies_list = driver.get_cookies()
         source = driver.page_source
-        print(source)
+        file = open("./html/"+url+".txt", "a")
+        file.write(source)
+        file.close()
         cookies_dict = {}
         bdar = 1
         for cookie in cookies_list:
@@ -41,7 +39,7 @@ def open_website_links(file_name):
           else: 
             bdar = 0
             break
-        f = open("BDAR_atitiktis.txt", "a")
+        f = open("atitiktis.txt", "a")
         if bdar == 0:
           print(f"{url} - {bcolors.FAIL}Netinka{bcolors.ENDC}")
           f.write(f"Netinka - {url}\n")
